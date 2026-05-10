a8b4b67d1d43d150253799fca9c0ca1e2dd26371
CruzAlanna <lannikcru@gmail.com>
2026-05-11 09:35:37 +1000

    CRUD: added show action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,4 +4,10 @@ class ContactsController < ApplicationController
 
     render json: contacts
   end
+
+  def show
+    contact = Contact.find_by(id: params[:id])
+
+    render json: contact
+  end
 end
