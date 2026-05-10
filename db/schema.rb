2305c8d41c8e3c0825ace9634536e5e7a550b73c
CruzAlanna <lannikcru@gmail.com>
2026-05-11 09:31:04 +1000

    CRUD: index action, seeded data

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_10_231406) do
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.integer "phone"
+    t.datetime "updated_at", null: false
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Contact.create(name: "Alanna Cruz", phone: 1234567890, email: "alanna@mail.com")
+
+Contact.create(name: "Lanna Cruz", phone: 2234567891, email: "lanna@mail.com")
+
+Contact.create(name: "Alan Cruz", phone: 3234567892, email: "alan@mail.com")
+
+Contact.create(name: "Anna Cruz", phone: 4234567893, email: "anna@mail.com")
+
+Contact.create(name: "Annala Cruz", phone: 5234567894, email: "annala@mail.com")
+
+
+# rails db:drop
+# rails db:create
+# rails db:migrate
+# rails db:seed
